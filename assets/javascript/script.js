c19f7ffcccd1ab1d92b3e489a09581b21501cf8a
Abdallah Day <10871403+AbdallahDay@users.noreply.github.com>
2018-08-21 01:12:56 -0500

    began integrating firebase

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,25 +1,81 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyBfbk7qQCvmuF6iwyKGjWf9nm7G1C7Id0o",
+authDomain: "trainscheduler-4b33a.firebaseapp.com",
+databaseURL: "https://trainscheduler-4b33a.firebaseio.com",
+projectId: "trainscheduler-4b33a",
+storageBucket: "trainscheduler-4b33a.appspot.com",
+messagingSenderId: "796002551874"
+};
 
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+function NextArrival_Time(first, frequency) {
+    var t = moment().diff(first, "minutes");
+    return moment(first).add(Math.ceil(t / frequency) * frequency);
+}
+
+function NextArrival_MinutesAway(first, frequency) {
+    var t = moment().diff(first, "minutes");
+    return frequency - (t % frequency);
+}
 
 $(document).ready(function() {
+    
     $("#btnSubmit").on("click", function() {
         var name = $("#name").val().trim();
-        var dest = $("#destination").val().trim();
-        var first = moment($("#firstTrain").val().trim(), "HH:mm");
+        var destination = $("#destination").val().trim();
+        var first = moment($("#first").val().trim(), "HH:mm");
         var frequency = Number.parseInt($("#frequency").val().trim());
 
         //TODO: perform form validation
         //name - empty
-        //dest - empty
+        //destination - empty
         //first - empty or invalid format (use first.isValid())
         //frequency - empty or NaN
 
         var train = {
             name: name,
-            destination: dest,
-            firstTrain: first,
+            destination: destination,
+            first: first,
             frequency: frequency
         }
+        console.log(train);
 
         //send to firebase
+        database.ref().push(train);
+
+        console.log("pushed to firebase");
+    });
+
+    database.ref().on("child_added", function(snapshot) {
+        var train = snapshot.val();
+
+        console.log("New child added:");
+        console.log(train);
+
+        var name = train.name;
+        var destination = train.destination;
+        var frequency = train.frequency;
+        var next = NextArrival_Time(train.first, train.frequency).format("hh:mm A");
+        var minsAway = NextArrival_MinutesAway(train.first, train.frequency);
+
+        var tr = $("<tr>");
+
+        var tdName = $(`<td>${name}</td>`);
+        var tdDestination = $(`<td>${destination}</td>`);
+        var tdFrequency = $(`<td>${frequency}</td>`);
+        var tdNext = $(`<td>${next}</td>`);
+        var tdMinsAway = $(`<td>${minsAway}</td>`);
+
+        tr.append(tdName);
+        tr.append(tdDestination);
+        tr.append(tdFrequency);
+        tr.append(tdNext);
+        tr.append(tdMinsAway);        
+
+        $("#tBody").append(tr);
     });
 });
